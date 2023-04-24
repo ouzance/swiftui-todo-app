f3fb38caa6f8544d088aeb8a1b1b9fa9a1452589
ouzance <oguzhan.cetin@d-teknoloji.com.tr>
2023-04-24 17:56:47 +0300

    Initial Commit

---
+++ swiftui-todo-app/swiftui_todo_appApp.swift
@@ -0,0 +1,17 @@
+//
+//  swiftui_todo_appApp.swift
+//  swiftui-todo-app
+//
+//  Created by oğuzhan çetin on 24.04.2023.
+//
+
+import SwiftUI
+
+@main
+struct swiftui_todo_appApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
